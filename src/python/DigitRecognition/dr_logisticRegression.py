d93dd1827280b9d4d4cc2074e5ed4b4f5e357966
chenyyx <chinachenyyx@163.com>
2017-08-04 13:20:05 +0800

    添加sklearn_lr的dr版本

--- src/python/DigitRecognition/dr_logisticRegression.py
+++ src/python/DigitRecognition/dr_logisticRegression.py
@@ -1,70 +1,67 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Created on 2017-7-25 14:03:45
+Created on 2017-08-03 14:04:32
 @author: ApacheCN_xy
+Description: DigitRecognizer for sklearn_logisticRegression
+Tips:this lr use time=4178s....,so many time aaaaaaaaaa!!!!! And the Kaggle Score=0.90786
+github: https://github.com/chenyyx/Kaggle
 """
-#!/usr/bin/python
+
 from numpy import *
-from os import listdir
+import csv
+import time
+import pandas as pd
 
 
-def loadData(direction):
-    trainfileList=listdir(direction)
-    m=len(trainfileList)
-    dataArray= zeros((m,1024))
-    labelArray= zeros((m,1))
-    for i in range(m):
-        returnArray=zeros((1,1024))  #每个txt文件形成的特征向量
-        filename=trainfileList[i]
-        fr=open('%s/%s' %(direction,filename))
-        for j in range(32):
-            lineStr=fr.readline()
-            for k in range(32):
-                returnArray[0,32*j+k]=int(lineStr[k])
-        dataArray[i,:]=returnArray   #存储特征向量
-    
-        filename0=filename.split('.')[0]
-        label=filename0.split('_')[0]
-        labelArray[i]=int(label)     #存储类别
-    return dataArray,labelArray
-    
-def sigmoid(inX):
-    return 1.0/(1+exp(-inX))
+# 加载数据
+def opencsv():#使用pandas打开      
+    data = pd.read_csv('input/DigitRecognition/train.csv')      
+    data1=pd.read_csv('input/DigitRecognition/test.csv')
+    train_data = data.values[0:,1:]#读入全部训练数据
+    train_label = data.values[0:,0]
+    test_data=data1.values[0:,0:]#测试全部测试个数据
+    return train_data,train_label,test_data
 
-#alpha:步长，maxCycles:迭代次数，可以调整
-def gradAscent(dataArray,labelArray,alpha,maxCycles):
-    dataMat=mat(dataArray)    #size:m*n
-    labelMat=mat(labelArray)      #size:m*1
-    m,n=shape(dataMat)
-    weigh=ones((n,1)) 
-    for i in range(maxCycles):
-        h=sigmoid(dataMat*weigh)
-        error=labelMat-h    #size:m*1
-        weigh=weigh+alpha*dataMat.transpose()*error
-    return weigh
 
-def classfy(testdir,weigh):
-    dataArray,labelArray=loadData(testdir)
-    dataMat=mat(dataArray)
-    labelMat=mat(labelArray)
-    h=sigmoid(dataMat*weigh)  #size:m*1
-    m=len(h)
-    error=0.0
-    for i in range(m):
-        if int(h[i])>0.5:
-            print int(labelMat[i]),'is classfied as: 1'
-            if int(labelMat[i])!=1:
-                error+=1
-                print 'error'
-        else:
-            print int(labelMat[i]),'is classfied as: 0'
-            if int(labelMat[i])!=0:
-                error+=1
-                print 'error'
-    print 'error rate is:','%.4f' %(error/m)
-                
-def digitRecognition(trainDir,testDir,alpha=0.07,maxCycles=10):
-    data,label=loadData(trainDir)
-    weigh=gradAscent(data,label,alpha,maxCycles)
-    classfy(testDir,weigh)
+def saveResult(result,csvName):
+    with open(csvName,'wb') as myFile:    
+        myWriter=csv.writer(myFile)
+        myWriter.writerow(["ImageId","Label"])
+        index=0
+        for i in result:
+            tmp=[]
+            index=index+1
+            tmp.append(index)
+            #tmp.append(i)
+            tmp.append(int(i))
+            myWriter.writerow(tmp)
+
+
+from sklearn.linear_model.logistic import LogisticRegression
+
+def lrClassify(trainData,trainLabel,testData):
+    lr = LogisticRegression()
+    lr.fit(trainData, ravel(trainLabel))
+    testLabel = lr.predict(testData)
+    saveResult(testLabel,'output/DigitRecognizer/Result_sklearn_lr.csv')
+    return testLabel
+
+def dRecognition_lr():
+    loadStartTime = time.time()
+    trainData,trainLabel,testData = opencsv()
+    # print "trainData==>", type(trainData), shape(trainData)
+    # print "trainLabel==>", type(trainLabel), shape(trainLabel)
+    # print "testData==>", type(testData), shape(testData)
+    loadEndTime=time.time()
+    print "load data finish"
+    print('load data time used:%f' % (loadEndTime - loadStartTime))
+    t = time.time()
+    result=lrClassify(trainData,trainLabel,testData)   
+    print "finish!"
+    k=time.time()
+    print('classify time used:%f' % (k - t))
+
+
+if __name__ == '__main__':
+    dRecognition_lr()
