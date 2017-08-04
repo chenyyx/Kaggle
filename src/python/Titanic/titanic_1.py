4f8a7d799dae56e72ea1e4d6332bb5fddf9398fe
chenyyx <chinachenyyx@163.com>
2017-08-04 18:12:15 +0800

    modify titanic_1.py

--- src/python/Titanic/titanic_1.py
+++ src/python/Titanic/titanic_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Created on 2017-9-1 11:47:54
+Created on 2017-8-4 17:51:20
 @author: ApacheCN_xy
 Description: machine Learning for disaster
 github: https://github.com/chenyyx/Kaggle
