5d97cc77e0800e2ba1dfb52eeeed15de469d16a8
chenyyx <chinachenyyx@163.com>
2017-08-18 16:33:59 +0800

    add a dr_c99_1.py file

---
+++ src/python/DigitRecognition/dr_c99_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Created on 2017-8-4 17:29:57
+@author: Joy yx
+Description: DigitRecognizer for 
+Tips: LoadData time use 8.984000s,and classify time use 15.467000s, the Kaggle Score=0.85729
+github: https://github.com/chenyyx/Kaggle
+"""
